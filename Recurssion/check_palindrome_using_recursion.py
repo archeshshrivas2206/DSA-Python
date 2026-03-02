e5c392961b4a91c73430b6cdb04ddb56f8c5ee94
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-03-02 19:39:03 +0530

    Palindrome using recursion

--- Recurssion/check_palindrome_using_recursion.py
+++ Recurssion/check_palindrome_using_recursion.py
@@ -3,6 +3,14 @@ left=0
 right=len(str)-1
 def palindrome(str,l,r):
     if l>=r:
-        return
+        return True
+    if str[l]!=str[r]:
+        return False
     palindrome(str,l+1,r-1)
+    return True
+if palindrome(str,left,right)==True:
+    print('It is an Palindrome')
+else:
+    print('It is not an Palindrome')
+
     
