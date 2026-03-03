635592fc9829ab73e30923f239f6e3f0e839d544
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-03-03 20:03:03 +0530

    insertion sort

---
+++ sorting algorithms/insertion_sort.py
@@ -0,0 +1,13 @@
+nums=[4,5,8,2,7,9,1,3,6]
+
+def insertion_sort_ascending(nums):
+    n=len(nums)
+    for i in range (1,n):
+        key=nums[i]
+        j=i-1
+        while j>=0 and nums[j]>key:
+            nums[j+1]=nums[j]
+            j-=1
+        nums[j+1]=key
+insertion_sort_ascending(nums)
+print(nums)
