0aaeea1d75e561dd06f1543940aae33f0ada7720
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-03-06 01:51:38 +0530

    random

--- sorting algorithms/merge_sort.py
+++ sorting algorithms/merge_sort.py
@@ -1,5 +1,5 @@
 arr=[3,5,9,7,2,8,1,4,6]
-def merge_array(l,r):
+def merge_array_ascending(l,r):
     result=[]
     i,j=0,0
     n,m=len(l),len(r)
@@ -10,12 +10,10 @@ def merge_array(l,r):
         else:
             result.append(r[j])
             j+=1
-    if i<n:
-        while i<n:
+    while i<n:
             result.append(l[i])
             i+=1
-    else:
-        while j<m:
+    while j<m:
             result.append(r[j])
             j+=1
     return result
@@ -25,7 +23,43 @@ def merge_sort_ascending(arr):
     mid=len(arr)//2
     left_arr=arr[:mid]
     right_arr=arr[mid:]
-    
+    left=merge_sort_ascending(left_arr)
+    right=merge_sort_ascending(right_arr)
+    return merge_array_ascending(left,right)
+def merge_array_descending(l,r):
+    result=[]
+    n=len(l)
+    m=len(r)
+    i,j=0,0
+    while i<n and j<m:
+        if l[i]>=r[j]:
+            result.append(l[i])
+            i+=1
+        else:
+            result.append(r[j])
+            j+=1
+
+    while i<n:
+            result.append(l[i])
+            i+=1
+    while j<m:
+            result.append(r[j])
+            j+=1
+    return result
+def merge_sort_descending(arr):
+    if len(arr)<=1:
+         return arr
+    mid =len(arr)//2
+    left_array=arr[:mid]
+    right_array=arr[mid:]
+    left=merge_sort_descending(left_array)
+    right=merge_sort_descending(right_array)
+    return merge_array_descending(left,right)
+         
+        
+print(merge_sort_ascending(arr))
+print(merge_sort_descending(arr))
+
 
 
     
---
+++ sorting algorithms/quick_sort.py
@@ -0,0 +1,4 @@
+arr=[9,5,1,3,7,4,6,8,2]
+
+def partition(arr,low,high):
+    pivot=arr[low]
