dffef54c9493edc64406d177ffd19b2c76f6647c
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-03-15 01:52:49 +0530

    done

--- list or array questions/righth_rotate_array_by_k_places.py
+++ list or array questions/righth_rotate_array_by_k_places.py
@@ -2,10 +2,24 @@ arr=[1,2,3,4,5,6,7,8,9]
 n=len(arr)
 k=int(input("Enter the rotations you want"))
 rotations=k%n
-def right_rotator_by_k_place_bruteforce(arr):
+
+def right_rotator_by_k_place_bruteforce(arr):#brute force method 
     for _ in range(0,rotations):#one way of doing rotation
         e=arr.pop()
         arr.insert(0,e)
 
-def right_rotator_by_k_place_slightlybetter(arr):
+def right_rotator_by_k_place_better(arr):# slicing 
+    arr[:]= arr[-rotations:] + arr[:-rotations]
+
     
+def reverser(arr,l,r):
+    while l<r:
+        arr[l],arr[r]=arr[r],arr[l]
+        l+=1
+        r-=1
+
+def right_rotator_by_k_place_optimal(arr):
+    reverser(n-k,n-1)#reversing the part that is supposed to be rotated and inserted at top
+    reverser(0,n-k-1)#reversing the remaining part
+    reverser(0,n-1)#reversing the whole array 
+
