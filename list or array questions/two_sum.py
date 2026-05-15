cbd5df693c419219ae43db2830c4c66107b490a7
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-05-15 18:04:00 +0530

    4th one done

--- list or array questions/two_sum.py
+++ list or array questions/two_sum.py
@@ -1,14 +1,22 @@
 nums=[5,9,1,2,4,15,6,3]
 target=13
-def bruteforce(nums):
+def bruteforce(nums,target):
     for i in range(0,len(nums)-1):
         for j in range (i+1,len(nums)):
             if nums[i]+nums[j]==target:
                 return [i,j]
             
-print(bruteforce(nums))
+print("this is bruteforce",bruteforce(nums,target))
 
 
-def optimal(nums):
+def optimal(nums,target):
     n=len(nums)
-    
+    freq={}
+    for i in range(0,n):
+        complement=target-nums[i]
+        if complement in freq:
+            return [freq[complement],i]
+        else:
+            freq[nums[i]]=i
+print(optimal(nums,target))
+
