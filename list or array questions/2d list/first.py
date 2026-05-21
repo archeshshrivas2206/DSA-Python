25c780b2153e6458e3225bd0b0b7135114ca2040
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-05-21 18:02:37 +0530

    2d start

---
+++ list or array questions/2d list/first.py
@@ -0,0 +1,8 @@
+#A 2d list is basically a list within a list 
+nums=[[1,2,3],[4,5,6],[7,8,9]]
+for i in range(0,len(nums)):
+    print(len(nums[i]))
+
+#how to iterate each element of 2d array
+count=0
+
